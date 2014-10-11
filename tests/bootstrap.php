b569f783f73443379455f7e4377e4422789b10c2
Mark Jaquith <mark@jaquith.me>
2014-10-11 04:55:18 -0400

    Update PHPUnit/Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,27 @@
 <?php
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../i-make-plugins.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require $_tests_dir . '/includes/bootstrap.php';
 
 class CWS_IMP_TestCase extends WP_UnitTestCase {
 	function plugin() {
 		return CWS_I_Make_Plugins::$instance;
+	}
+
+	function set_post( $key, $value ) {
+		$_POST[$key] = $_REQUEST[$key] = addslashes( $value );
+	}
+
+	function unset_post( $key ) {
+		unset( $_POST[$key], $_REQUEST[$key] );
 	}
 }
